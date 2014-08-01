0e28ec3cc299cf31d005bdca0ba59dea93e2efc4
Bistra Yakimova <bistra.yakimova@gmail.com>
2014-08-01 17:29:30 +0300

    Introduce localization file for bulgarian and Future version of php files using the localized strings. php friendly

---
+++ public_html/locale/dances.php
@@ -0,0 +1,87 @@
+п»ї<?php
+    $page = 'Dances';
+    include('templates/header.php');
+?>
+
+<div class="content-wrapper">
+
+	<div class ="text-wrapper">
+		<div class="main-content">
+			<h1 class="page-title">gettext("DANCES_TITLE")</h1>
+			<h2>gettext("DANCES_DESCRIPTION")</h2>
+				<div class="dance-style clearfix">
+					<div class="image-wrapper-2">
+						<img src="images/wedding_dance.jpg" alt="Хореография на първи сватбен танц">
+					</div><!-- end of .image-wrapper-2 -->
+					<div class="pararg-wrapper-2">
+						<h3>gettext("DANCES_FIRST_WEDDING_TITLE")</h3>
+						<p>gettext("DANCES_FIRST_WEDDING_DESCRIPTION")</p>
+					</div><!-- end of .pararg-wrapper-2 -->
+				</div><!-- end of .dance-style -->
+				<div class="dance-style-right clearfix">
+					<div class="image-wrapper-2">
+						<img src="images/kumove.jpg" alt="Хореография на кумовете">
+					</div><!-- end of .image-wrapper-2 -->
+					<div class="pararg-wrapper-2">
+						<h3>gettext("DANCES_BESTMEN_DANCE_TITLE")</h3>
+						<p>gettext("DANCES_BESTMEN_DANCE_DESCRIPTION")</p>
+					</div><!-- end of .pararg-wrapper-2 -->
+				</div><!-- end of .dance-style -->
+				<div class="dance-style clearfix">
+					<div class="image-wrapper-2">
+						<img src="images/pozdrav.jpg" alt="Танц – поздрав на важно събитие">
+					</div><!-- end of .image-wrapper-2 -->
+					<div class="pararg-wrapper-2">
+						<h3>gettext("DANCES_SALUTATION_DANCE_TITLE")</h3>
+						<p>gettext("DANCES_SALUTATION_DANCE_DESCRIPTION")</p>
+					</div><!-- end of .pararg-wrapper-2 -->
+				</div><!-- end of .dance-style -->
+				<div class="dance-style-right clearfix">
+					<div class="image-wrapper-2">
+						<img src="images/narodni.png" alt="Български народни танци">
+					</div><!-- end of .image-wrapper-2 -->
+					<div class="pararg-wrapper-2">
+						<h3>gettext("DANCES_BULGARIAN_FOLKLORE_TITLE")</h3>
+						<p>gettext("DANCES_BULGARIAN_FOLKLORE_DESCRIPTION1")</p>
+						<p>gettext("DANCES_BULGARIAN_FOLKLORE_DESCRIPTION2")</p>
+					</div><!-- end of .pararg-wrapper-2 -->
+				</div><!-- end of .dance-style -->
+				<div class="dance-style clearfix">
+					<div class="image-wrapper-2">
+						<img src="images/static.jpg" alt="Статични танци –а.к.а. фотография">
+					</div><!-- end of .image-wrapper-2 -->
+					<div class="pararg-wrapper-2">
+						<h3>gettext("DANCES_PHOTOGRAPHY_TITLE")</h3>
+						<p>gettext("DANCES_PHOTOGRAPHY_DESCRIPTION")</p>
+					</div><!-- end of .pararg-wrapper-2 -->
+				</div><!-- end of .dance-style -->
+				<div class="dance-style-right clearfix">
+					<div class="image-wrapper-2">
+						<img src="images/king_and_queen.jpg" alt="Искам да съм крал/кралица на бала">
+					</div><!-- end of .image-wrapper-2 -->
+					<div class="pararg-wrapper-2">
+						<h3>gettext("DANCES_PROM_DANCE_TITLE")</h3>
+						<p>gettext("DANCES_PROM_DANCE_DESCRIPTION")</p>
+					</div><!-- end of .pararg-wrapper-2 -->
+				</div><!-- end of .dance-style -->
+				<div class="dance-style clearfix">
+					<div class="image-wrapper-2">
+						<img src="images/logo_supernova-2.png" alt="Какво ще предложите вие?">
+					</div><!-- end of .image-wrapper-2 -->
+					<div class="pararg-wrapper-2">
+						<h3>gettext("DANCES_SUGGESTIONS_TITLE")</h3>
+						<p>gettext("DANCES_SUGGESTIONS_DESCRIPTION")</p><br />
+					</div><!-- end of .pararg-wrapper-2 -->
+				</div><!-- end of .dance-style -->
+			
+    	</div> <!-- end of .main-content -->
+    </div> <!-- end of .text-wrapper -->
+
+<?php include('templates/aside.php') ?>
+
+</div>
+
+<?php include('templates/footer.php') ?>
+
+
+
---
+++ public_html/locale/index.php
@@ -0,0 +1,37 @@
+﻿<?php
+    $page = 'index';
+    include('templates/header.php');
+?>
+    <div class="content-wrapper">
+	    <div class ="text-wrapper">
+			<div class="main-content">
+				<h1 class="page-title">gettext("INDEX_TITLE")</h1>
+				<div class="pararg-wrapper">
+					<p>
+					<span class="emphasized">gettext("INDEX_APPLAUS_DANCE")
+					</p>
+					<p>
+					<span class="emphasized">gettext("INDEX_APPLAUS_DANCE1")
+					</p>
+				</div><!-- end of .pararg-wrapper -->
+				<div class="image-wrapper">
+					<img src="images/bibi_mitio_300_200.png" alt="Image">
+					<img src="images/bobi_ive_300_200.png" alt="Image">
+					<img src="images/bibi_nace_300_200.png" alt="Image">
+				</div><!-- end of .image-wrapper -->
+				<div class="pararg-wrapper">
+					<h2>gettext("INDEX_TITLE2")</h2>
+					<p>gettext("INDEX_APPLAUS_DANCE2")</p>
+				</div><!-- end of .pararg-wrapper -->
+				<div class="image-wrapper">
+					<img src="images/bibi_poza_200_300.png" alt="Image">
+					<img src="images/iva_slavi_500_300.png" alt="Image">
+					<img src="images/petya_marto_200_300.png" alt="Image">
+				</div><!-- end of .image-wrapper -->
+                        
+    		</div> <!-- end of .main-content -->
+    	</div> <!-- end of .text-wrapper -->
+
+	<?php include('templates/aside.php') ?>
+	</div>
+<?php include('templates/footer.php') ?>
