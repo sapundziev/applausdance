26f7a1ce1f05fe65e420abc63142988896e1ba1d
Nace Sapundziev <n.sapundziev@gmail.com>
2014-08-04 13:43:32 +0300

    Enable gallery
    Change-Id: I531bf11d7aa2fff512a23fe876fb451aa51e1dfb

---
+++ public_html/gallery-1.php
@@ -0,0 +1,84 @@
+<?php
+    $page = 'Dances';
+    include('templates/header.php');
+?>
+
+<div class="content-wrapper">
+
+    <div class ="text-wrapper">
+        <div class="main-content clearfix">
+            <h1 class="page-title">Hoverbox Image Gallery</h1>
+            <ul class="hoverbox">
+                <li>
+                    <a href="#"><img src="images/demo/photo01.jpg" alt="description" /><img src="images/demo/photo01.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo02.jpg" alt="description" /><img src="images/demo/photo02.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo03.jpg" alt="description" /><img src="images/demo/photo03.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo04.jpg" alt="description" /><img src="images/demo/photo04.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo05.jpg" alt="description" /><img src="images/demo/photo05.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo06.jpg" alt="description" /><img src="images/demo/photo06.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo07.jpg" alt="description" /><img src="images/demo/photo07.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo08.jpg" alt="description" /><img src="images/demo/photo08.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo09.jpg" alt="description" /><img src="images/demo/photo09.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo10.jpg" alt="description" /><img src="images/demo/photo10.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo11.jpg" alt="description" /><img src="images/demo/photo11.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo12.jpg" alt="description" /><img src="images/demo/photo12.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo13.jpg" alt="description" /><img src="images/demo/photo13.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo14.jpg" alt="description" /><img src="images/demo/photo14.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo15.jpg" alt="description" /><img src="images/demo/photo15.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo16.jpg" alt="description" /><img src="images/demo/photo16.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo17.jpg" alt="description" /><img src="images/demo/photo17.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo18.jpg" alt="description" /><img src="images/demo/photo18.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo19.jpg" alt="description" /><img src="images/demo/photo19.jpg" alt="description" class="preview" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="images/demo/photo20.jpg" alt="description" /><img src="images/demo/photo20.jpg" alt="description" class="preview" /></a>
+                </li>
+            </ul>
+        </div><!-- end of .main-content -->      
+            
+    </div> <!-- end of .text-wrapper -->
+
+<?php include('templates/aside.php') ?>
+
+</div><!-- end of .content-wrapper -->
+
+<?php include('templates/footer.php') ?>
+
+
+
