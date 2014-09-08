ce65bb751d5724085274bc240d5647150bb25557
Nace Sapundziev <n.sapundziev@gmail.com>
2014-09-08 11:15:28 +0300

    Add google analytics
    Change-Id: I903a8db3454de1cf94dac9a5bcf15492443ed3cb

--- public_html/templates/footer.php
+++ public_html/templates/footer.php
@@ -7,25 +7,6 @@
 
     </footer>
 </div><!-- end of .wrapper -->
-
-<!--    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-    <script>
-        if (typeof jQuery == 'undefined') {
-        var e = document.createElement('script');
-        e.src = "js/libs/jquery-1.8.3.min.js";
-        e.type='text/javascript';
-        document.getElementsByTagName("head")[0].appendChild(e);
-        }
-    </script>
-
-
-    <script src="js/plugins.js"></script>
-    <script src="js/application.js"></script>-->
-
-    <!-- end scripts -->
-
-    <!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID.
-       mathiasbynens.be/notes/async-analytics-snippet -->
         <script src="js/application.js"></script>
 </body>
 </html>
